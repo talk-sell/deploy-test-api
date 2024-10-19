2557aa87e0a7895956c3815957301127e82d72df
talk-sell <valentin.olaru.007@gmail.com>
2024-10-19 11:01:42 +0300

    update code
    Signed-off-by: talk-sell <valentin.olaru.007@gmail.com>

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ namespace deploy_test_api.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),  // Replaced DateOnly with DateTime
+                Date = DateTime.Now.AddDays(index),  // Using DateTime
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
